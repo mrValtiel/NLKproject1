72d57a1240f38dae7928b019f03d74e3935b9a3d
mrValtiel <mrvaltiel@gmail.com>
2018-12-04 20:18:15 +0100

    Speech recognition

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import re
+import speech_text as st
+
+#t = st.getSpeechCommand()
+#print(t) #{'success': True, 'error': None, 'transcription': 'hello and welcome my friend'}
+
+choice = 0
+commandsHistory = []
+
+while (choice != -1):
+    print("*************************************************************")
+    print("Welcome to Human Console, please start by choosing an option:")
+
+
+print("Goodbye")
---
+++ speech_text.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+
+def getSpeechCommand():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("Say command")
+        audio = r.listen(source)
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = r.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
